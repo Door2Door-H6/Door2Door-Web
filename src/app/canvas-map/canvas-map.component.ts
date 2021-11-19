62d1a8d619237d6ef707b0ecfcb09be540d94d6e
Andi Olle Olsen <zbcanols21@zbc.dk>
2021-11-19 19:12:11 +0100

    added a canvas and a boutten to draw

--- src/app/canvas-map/canvas-map.component.ts
+++ src/app/canvas-map/canvas-map.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-canvas-map',
   templateUrl: './canvas-map.component.html',
   styleUrls: ['./canvas-map.component.scss']
 })
-export class CanvasMapComponent implements OnInit {
+export class CanvasMapComponent implements AfterViewInit {
 
+  @ViewChild('canvas') canvas: ElementRef<HTMLCanvasElement>
+  public context: CanvasRenderingContext2D;
   constructor() { }
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    this.context = this.canvas.nativeElement.getContext('2d');
+  }
+
+  drawRectable() {
+
+      this.context.fillStyle = "#D74022";
+      this.context.fillRect(25, 25, 150, 150);
+
+      this.context.fillStyle = "rgba(0,0,0,0.5)";
+      this.context.clearRect(60, 60, 120, 120);
+      this.context.strokeRect(90, 90, 80, 80);
+
+
   }
 
 }
