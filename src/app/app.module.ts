b7a79d58a57e9d00bc9e7a78efbbc2fc8b717c1a
Andi Olle Olsen <zbcanols21@zbc.dk>
2021-11-19 18:56:38 +0100

    start Gen componets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TabsContainerComponent } from './tabs-container/tabs-container.component';
+import { CanvasMapComponent } from './canvas-map/canvas-map.component';
+import { CategorysComponent } from './categorys/categorys.component';
+import { FrontpageComponent } from './frontpage/frontpage.component';
+
+// import {} from '@angular/material'
+import {MatTabsModule} from '@angular/material/tabs'
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TabsContainerComponent,
+    CanvasMapComponent,
+    CategorysComponent,
+    FrontpageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatTabsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/canvas-map/canvas-map.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-canvas-map',
+  templateUrl: './canvas-map.component.html',
+  styleUrls: ['./canvas-map.component.scss']
+})
+export class CanvasMapComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
