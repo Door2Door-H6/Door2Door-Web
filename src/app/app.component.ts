299cb51be2fb422752e9095eab4ff6b9330e0a6f
goket1 <40742575+goket1@users.noreply.github.com>
2021-11-25 13:17:16 +0100

    Added leaflet map with ZBC map, and test data. (#1)
    Refactored and moved everything from app.component to frontpage.component and added routes
    
    Co-authored-by: user <a@a.a>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,19 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  public qrdata: string = null;
-  public elementType: "img" | "url" | "canvas" | "svg" = null;
-  public level: "L" | "M" | "Q" | "H";
-  public scale: number;
-  public width: number;
-
   constructor() {
-    this.elementType = "img";
-    this.level = "M";
-    this.qrdata = "www.door2door.com";
-    this.scale = 1;
-    this.width = 64;
-
   }
 
   title = 'Door2Door';
--- src/app/frontpage/frontpage.component.ts
+++ src/app/frontpage/frontpage.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-frontpage',
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FrontpageComponent implements OnInit {
 
-  constructor() { }
+  public qrdata: string = null;
+  public elementType: 'img' | 'url' | 'canvas' | 'svg' = null;
+  public level: 'L' | 'M' | 'Q' | 'H';
+  public scale: number;
+  public width: number;
+
+  constructor() {
+    this.elementType = 'img';
+    this.level = 'M';
+    this.qrdata = 'https://door2door.dk';
+    this.scale = 1;
+    this.width = 128;
+
+  }
 
   ngOnInit(): void {
   }
-
 }
