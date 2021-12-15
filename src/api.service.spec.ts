25ad90d35270d4f21b41386c796da9f0a45ce1e9
user <a@a.a>
2021-12-15 17:21:27 +0100

    test

--- src/api.service.spec.ts
+++ src/api.service.spec.ts
@@ -1,5 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import {HttpClient} from '@angular/common/http';
 
 import { ApiService } from './api.service';
 
